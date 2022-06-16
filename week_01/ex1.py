d7ef7bca4bc022b5ae7d6ea69932baeb57505218
Siwakon <tiger.prasang@gmail.com>
2022-06-16 22:33:32 +0700

    update

--- week_01/ex1.py
+++ week_01/ex1.py
@@ -11,7 +11,13 @@ vec_2 = np.array(list_2)
 print(list_1 + list_2)
 print(vec_1 * vec_2)
 print(np.dot(vec_1,vec_2))
+
+Result
+[1, 2, 3, 4, 2, 3, 4, 5]
+[ 2  6 12 20]
+40
 """
+
 
 a = np.array([1,2,0.5])
 b = np.arange(0,10)
@@ -23,13 +29,19 @@ print(a)
 print(b)
 print(c)
 print(d)
+
+Result
+[1.  2.  0.5]
+[0 1 2 3 4 5 6 7 8 9]
+[0 2 4 6 8]
+[ 0.  1.  2.  3.  4.  5.  6.  7.  8.  9. 10.]
 """
+
+
 
 N = 100
 t = np.linspace(0, 10, N+1)
 y = np.sin(t)
-
-#print(y)
 
 """
 plt.plot(t,y)
@@ -39,8 +51,8 @@ plt.xlabel("time [s]")
 plt.show()
 """
 
-#print(np.linalg.norm(a))
-#print(a/np.linalg.norm(a))
+print(np.linalg.norm(a))
+print(a/np.linalg.norm(a))
 
 a_radom = np.random.rand((12))
 A = np.array([[1,2,3,4,5],[5,6,7,8,9]])
@@ -69,7 +81,11 @@ X = np.array([[0,0,1]]).T
 Y = np.array([[0,-1,0]]).T
 Z = np.array([[1,0,0]]).T
 Result = np.concatenate((X,Y,Z), axis=1)
+
+"""
 print(Result)
+"""
+
 
 
 
